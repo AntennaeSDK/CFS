0603a12edd7bf7a179c5ae4f2a915347b99fe0ee
snambi <snambi@gmail.com>
2017-06-23 19:40:52 -0700

    Added Mysql DB and configuration

---
+++ src/main/scripts/db_user.sql
@@ -0,0 +1,8 @@
+
+-- Create userids
+GRANT ALL PRIVILEGES ON *.* TO 'cfs'@'localhost' IDENTIFIED BY 'cfs123';
+GRANT ALL PRIVILEGES ON *.* TO 'cfs'@'*' IDENTIFIED BY 'cfs123';
+
+-- Create Database
+CREATE DATABASE CFS;
+
