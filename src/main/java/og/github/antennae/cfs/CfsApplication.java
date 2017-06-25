55cc413e834c5efbb85140b844796e9c568bd401
snambi <snambi@gmail.com>
2017-06-25 14:40:27 -0700

    Added static content, bootstrap

--- src/main/java/og/github/antennae/cfs/CfsApplication.java
+++ src/main/java/og/github/antennae/cfs/CfsApplication.java
@@ -6,18 +6,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+/**
+ * Starts the application.
+ * NOTE : DO not add @EnableWebMvc it breaks static content delivery ( no idea why )
+ */
 @SpringBootApplication
-@EnableWebMvc
-public class CfsApplication{ //extends SpringBootServletInitializer {
+//@EnableWebMvc
+public class CfsApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfsApplication.class, args);
 	}
-
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(CfsApplication.class);
-//	}
 
 }
